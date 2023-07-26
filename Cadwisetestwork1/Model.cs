0bd7072dadc2b60ec1c7b8ee63a3d079064c7d4a
OneOfDiscoverer <OneOfDiscoverer@MAINPC>
2023-07-27 04:46:36 +0700

    Добавьте файлы проекта.

---
+++ Cadwisetestwork1/Model.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Cadwisetestwork1
+{
+    public class Parser : INotifyPropertyChanged
+    {
+        string path;
+        public string Path { get { return path; } set { path = value; OnPropertyChanged("Path"); } }
+        string outPath;
+        string sights;
+        int minLen = 0;
+        bool del;
+        bool rep;
+        double progress;
+        public double Progress { get { return progress; } set { progress = value; OnPropertyChanged("Progress"); } }
+        public Parser(string path, string outpath, string sights, int minlen, bool del, bool rep)
+        {
+            this.Path = path;
+            this.outPath = outpath;
+            this.sights = sights;
+            this.minLen = minlen;
+            this.del = del;
+            this.rep = rep;
+        }
+        public bool Start()
+        {
+            if (Path == null || outPath == null || sights == null)
+            {
+                return false;
+            }
+            else
+            {
+                var t = Task.Run(() => Do_work()); //ждать не будем
+                return true;
+            }
+        }
+        //private void Do_work_stream() //жирный, медленный, но рабочий метод
+        //{
+        //    using StreamReader sr = File.OpenText(path);
+        //    if (File.Exists(outPath))
+        //    {
+        //        File.Delete(outPath);
+        //    }
+        //    using StreamWriter sw = File.CreateText(outPath);
+        //    string word = "";
+        //    while (true)
+        //    {
+        //        int tmp = sr.Read();
+        //        Progress = 100 * (double)sr.BaseStream.Position / sr.BaseStream.Length;
+        //        var ch = () => { foreach (char i in sights) { if (tmp == i) return true; } return false; };
+        //        if (EndOfWorld((char)tmp, sights + ' ' + '\n'))
+        //        {
+        //            if (word.Length >= minLen && minLen > 0)
+        //            {
+        //                sw.Write(word, 0, word.Length);
+        //            }
+        //            if((char)tmp == '\n')
+        //            {
+        //                sw.Write('\n');
+        //            }
+        //            word = "";
+        //        }
+        //        if (ch() && del)
+        //        {
+        //            //if (rep)
+        //            //{
+        //            //    word += ' ';
+        //            //}
+        //        }
+        //        else
+        //        {
+        //            word += (char)tmp;
+        //        }
+        //        if (tmp == -1)
+        //        {
+        //            return;
+        //        }
+        //    }
+        //}
+        //private bool EndOfWorld(char ch, string str)
+        //{
+        //    foreach (char i in str) { if (ch == i) return true; } return false;
+        //}
+        private void Do_work() 
+        {
+            int cnt = 0, mpl = 1;
+            bool isSeeked = true;
+            using FileStream file = File.Open(path, FileMode.Open);
+            if (File.Exists(outPath))
+            {
+                File.Delete(outPath);
+            }
+            using FileStream opt = File.Open(outPath, FileMode.Create);
+            
+            while (true)
+            {
+                Progress = 100 * (double)file.Position / file.Length;
+                int tmp = file.ReadByte();
+                var ch = () => { foreach (char i in sights) { if (tmp == i) return true; } return false; };
+                cnt++;
+                if (tmp == ' ' || ch() || tmp == -1 || tmp == '\n')
+                {
+                    int tmpLen = minLen * mpl;
+                    if (cnt < tmpLen && tmpLen > 0 && cnt != 1)
+                    {
+                        opt.Seek(-cnt + 1, SeekOrigin.Current);
+                        isSeeked = false;
+                    }
+                    cnt = 0;
+                }
+                if (ch() && del)
+                {
+                    if (rep)
+                    {
+                        opt.WriteByte((byte)' ');
+                    }
+                }
+                else
+                {
+                    if (isSeeked || tmp != ' ')
+                    {
+                        opt.WriteByte((byte)tmp);
+                    }
+                    isSeeked = true;
+                }
+                if (((byte)tmp & 0x80) == 0x00) mpl = 1;
+                else if (((byte)tmp & 0xE0) == 0xC0) mpl = 2;
+                else if (((byte)tmp & 0xF0) == 0xE0) mpl = 3;
+                else if (((byte)tmp & 0xF8) == 0xF0) mpl = 4;
+                if (tmp == -1) 
+                {
+                    opt.SetLength(opt.Position -1);
+                    return;
+                }
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ Cadwisetestwork1/ViewModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.DirectoryServices;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Cadwisetestwork1
+{
+    public class ViewModel: INotifyPropertyChanged
+    {
+        Window1 wnd;
+        public int Len { get; set; } = 0;
+        bool del, rep;
+        public bool Del { get { return del; } set { del = value; DelSet = del; OnPropertyChanged("DelSet"); OnPropertyChanged("Rep"); } }
+        public bool DelSet { get; set; }
+        public bool Rep { get { return rep & del; } set { rep = value; } }
+        public string sights = ";.,:-";
+        public string Sights { get { return sights; } set { sights = value; OnPropertyChanged("Sights"); } }
+        public bool startSet = false;
+        public bool StartSet { get { return startSet; } set { startSet = value; OnPropertyChanged("StartSet"); } }
+        public string? path = null, outpath = null;
+        public string? Path { get { return path; } set { path = value; if (outpath != null) StartSet = true; else StartSet = false; OnPropertyChanged("Path"); } }
+        public string? OutPath { get { return outpath; } set { outpath = value; if (path != null) StartSet = true; else StartSet = false; OnPropertyChanged("Path"); } }
+        public ObservableCollection<Parser> Parsers { get; set; }
+        public ViewModel()
+        {
+            Parsers = new ObservableCollection<Parser>();
+        }
+        public RelayCommand Show
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    wnd = new Window1(this);
+                    wnd.Show();
+                });
+            }
+        }
+        public RelayCommand Source
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                { 
+                    var OpenDialog = new Microsoft.Win32.OpenFileDialog();
+                    OpenDialog.Filter = "Text documents (.txt)|*.txt";
+                    if (OpenDialog.ShowDialog() == true)
+                    {
+                        Path = OpenDialog.FileName;
+                    }
+                });
+            }
+        }
+        public RelayCommand OutFile
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    var SaveDialog = new Microsoft.Win32.SaveFileDialog();
+                    SaveDialog.Filter = "Text documents (.txt)|*.txt";
+                    if (SaveDialog.ShowDialog() == true)
+                    {
+                        if (SaveDialog.FileName == Path)
+                        {
+                            MessageBox.Show("Файл источник должен отличаться от файла назначения");
+                        }
+                        else
+                        {
+                            OutPath = SaveDialog.FileName;
+                        }
+
+                    }
+                });
+            }
+        }
+        public RelayCommand Start
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Parsers.Add(new Parser(Path, OutPath, sights, Len, Del, Rep));
+                    Path = OutPath = null;
+                    if(Parsers.Last().Start() == false)
+                    {
+                        MessageBox.Show("Выберите файл источник и файл приемник");
+                    }
+                    wnd.Close();
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
